b19124eaa9c3761934fba97b0127113949d336e2
yvonnewuhanzhi <hw3366@nyu.edu>
2024-02-14 15:28:19 +0800

    start

--- addMeshes.js
+++ addMeshes.js
@@ -1,75 +1,42 @@
 import {
+	TorusKnotGeometry,
+    TorusGeometry,
+	ConeGeometry,
 	BoxGeometry,
 	MeshBasicMaterial,
 	MeshStandardMaterial,
 	MeshLambertMaterial,
 	MeshPhysicalMaterial,
 	Mesh,
-	SphereGeometry,
+    SphereGeometry,
 	TextureLoader,
 } from 'three'
 
-// export const addBoilerPlateMeshes = () => {
-// 	const box = new BoxGeometry(1, 1, 1)
-// 	const boxMaterial = new MeshBasicMaterial({ color: 0xff0000 })
-// 	const boxMesh = new Mesh(box, boxMaterial)
-// 	boxMesh.position.set(-2, 0, 0)
-// 	return boxMesh
-// }
 
-// export const addStandardMesh = () => {
-// 	const box = new BoxGeometry(1, 1, 1)
-// 	const boxMaterial = new MeshStandardMaterial({ color: 0x00ff00 })
-// 	const boxMesh = new Mesh(box, boxMaterial)
-// 	boxMesh.position.set(2, 0, 0)
-// 	return boxMesh
-// }
 const loader = new TextureLoader();
 
-
-
-export const centerPlanet = () => {
-	
-	const color = loader.load('/textures/Blue_Ice_001_COLOR.jpg')
-	const displace = loader.load('/textures/Ice_001_DISP.png')
-	const normal = loader.load('/textures/Ice_001_NRM.jpg')
-	const centerPlanetGeometry = new SphereGeometry(0.5, 32, 16)
-	const centerPlanetMaterial = new MeshPhysicalMaterial({
-	map: color,
-	displacementMap: displace,
-	normalMap: normal,
-	metalness: 0.0,
-	roughness: 0.1,
-	thickness: 1.5,
-	})
-	const centerPlanetMesh = new Mesh(
-	centerPlanetGeometry,
-	centerPlanetMaterial
-	)
-	return centerPlanetMesh
-	}
-
 export const planet1 = () => {
-	const planet1Geometry = new SphereGeometry(0.2, 32, 16)
-	const planet1Material = new MeshStandardMaterial({ color: 0xa6e0b3, metalness: 0.6, roughness: 1.0 })
-	const planet1Mesh = new Mesh(planet1Geometry, planet1Material)
-	return planet1Mesh
-}
+    const planet1Geometry = new BoxGeometry(1, 0.4, 1); // 使用 BoxGeometry 创建一个立方体
+    const planet1Material = new MeshBasicMaterial({ color: 0xE2AFC1});
+    const planet1Mesh = new Mesh(planet1Geometry, planet1Material);
+    return planet1Mesh;
+};
 export const planet2 = () => {
-	const planet2Geometry = new SphereGeometry(0.3, 32, 16)
-	const planet2Material = new MeshPhysicalMaterial({ color: 0xa8e3da, sheen:0.7})
-	const planet2Mesh = new Mesh(planet2Geometry, planet2Material)
-	return planet2Mesh
-}
+    const planet2Geometry = new BoxGeometry(1, 2, 1); // 使用 BoxGeometry 创建一个立方体
+    const planet2Material = new MeshBasicMaterial({ color: 0xE84F78 });
+    const planet2Mesh = new Mesh(planet2Geometry, planet2Material);
+    return planet2Mesh;
+};
 export const planet3 = () => {
-	const planet3Geometry = new SphereGeometry(0.1, 32, 16)
-	const planet3Material = new MeshLambertMaterial({ color: 0xf8f7ff, emissive: 0xe398e3})
-	const planet3Mesh = new Mesh(planet3Geometry, planet3Material)
-	return planet3Mesh
-}
+    const planet3Geometry = new TorusGeometry(3, 0.5, 2, 4); // 创建圆环几何体
+    const planet3Material = new MeshBasicMaterial({ color: 0xbfd4f2 });
+    const planet3Mesh = new Mesh(planet3Geometry, planet3Material);
+    planet3Mesh.position.set(-4, -3, 0);
+    return planet3Mesh;
+};
 export const planet4 = () => {
-	const planet4Geometry = new SphereGeometry(0.4, 16, 16)
-	const planet4Material = new MeshBasicMaterial({ color: 0xe8f5b0,wireframe:true })
-	const planet4Mesh = new Mesh(planet4Geometry, planet4Material)
-	return planet4Mesh
-}
+    const planet4Geometry = new ConeGeometry( 4, 4.5, 3 );  
+    const planet4Material = new MeshBasicMaterial({ color: 0x499DAE});
+    const planet4Mesh = new Mesh(planet4Geometry, planet4Material);
+    return planet4Mesh;
+};
--- main.js
+++ main.js
@@ -1,77 +1,74 @@
 import './style.css'
 import * as THREE from 'three'
-import { centerPlanet,planet1,planet2,planet3,planet4 } from './addMeshes'
-// import { addBoilerPlateMeshes, addStandardMesh } from './addMeshes'
-import { addLight } from './addLights'
+import { planet1,planet2,planet3,planet4} from './addMeshes'
+
 
 const renderer = new THREE.WebGLRenderer()
 const camera = new THREE.PerspectiveCamera(
-	75,
-	window.innerWidth / window.innerHeight,
-	0.1,
-	100
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    100
 )
 const scene = new THREE.Scene()
 const meshes = {}
 const lights = {}
 const clock = new THREE.Clock() //clock
+scene.background = new THREE.Color(0x95c2cc);
 
 init()
 function init() {
-	//set up our renderer default settings, add scene/canvas to webpage
-	renderer.setSize(window.innerWidth, window.innerHeight)
-	document.body.appendChild(renderer.domElement)
-    meshes.center = centerPlanet()
-	meshes.planet1 = planet1()
+    
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+    meshes.planet1 = planet1()
 	meshes.planet2 = planet2()
 	meshes.planet3 = planet3()
 	meshes.planet4 = planet4()
-	// meshes.default = addBoilerPlateMeshes()
-	// meshes.standard = addStandardMesh()
-	lights.default = addLight()
 
-	scene.add(lights.default)
-	scene.add(meshes.center)
-	scene.add(meshes.planet1)
-	scene.add(meshes.planet2)
-	scene.add(meshes.planet3)
-	scene.add(meshes.planet4)
-	// scene.add(meshes.standard)
-	// scene.add(meshes.default)
+ 
 
-	camera.position.set(0, 0, 5)
-	resize()
-	animate()
+    scene.add(meshes.planet1)
+    scene.add(meshes.planet2)
+    scene.add(meshes.planet3)
+    scene.add(meshes.planet4)
+	
+
+
+    camera.position.set(0, 0, 5)
+    resize()
+    animate()
 }
 
 function resize() {
-	window.addEventListener('resize', () => {
-		renderer.setSize(window.innerWidth, window.innerHeight)
-		camera.aspect = window.innerWidth / window.innerHeight
-		camera.updateProjectionMatrix()
-	})
+    window.addEventListener('resize', () => {
+        renderer.setSize(window.innerWidth, window.innerHeight)
+        camera.aspect = window.innerWidth / window.innerHeight
+        camera.updateProjectionMatrix()
+    })
 }
 
 function animate() {
-	requestAnimationFrame(animate)
-    //CLOCK
-	const tick = clock.getElapsedTime()
-	meshes.planet1.position.x=Math.sin(tick)*4
-	meshes.planet1.position.y=Math.cos(tick)*4
+    requestAnimationFrame(animate);
 
-	meshes.planet2.position.x=Math.cos(tick)*3
-	meshes.planet2.position.y=Math.sin(tick)*2
-	
-	meshes.planet3.position.x=Math.sin(tick)
-	meshes.planet3.position.y=Math.cos(tick)
-	
-	meshes.planet4.position.x=-Math.sin(tick)*6
-	meshes.planet4.position.y=-Math.cos(tick)*5.5
+    const tick = clock.getElapsedTime();
+    const speed1 = 0.05; 
+    const speed2 = 0.08; 
+    const speed3 = 0.1; 
+    const speed4 = 0.06
 
-	// meshes.default.rotation.x += 0.01
-	// meshes.default.rotation.y -= 0.01
-	// meshes.standard.rotation.x -= 0.01
-	// meshes.standard.rotation.z -= 0.01
+    // 计算每个星球的位置
+    meshes.planet1.position.x = Math.sin(tick * speed1) * 3; 
+    meshes.planet1.position.y = Math.cos(tick * speed1) * 3;
 
-	renderer.render(scene, camera)
+    meshes.planet2.position.x = Math.cos(tick * speed2) * 3; 
+    meshes.planet2.position.y = Math.sin(tick * speed2) * 3;
+
+    meshes.planet3.position.x = Math.sin(tick * speed3) -3.5
+    meshes.planet3.position.y = Math.cos(tick * speed3) -3
+
+    meshes.planet4.position.x = Math.sin(tick * speed4) 
+    meshes.planet4.position.y = Math.cos(tick * speed4) 
+
+    renderer.render(scene, camera);
 }
